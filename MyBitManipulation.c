ecc7b9b462a27d83dd180cca3f76e470d8a0ff88
gordonzhang-sbu <gordon.zhang@stonybrook.edu>
2026-02-18 18:19:57 +0000

    Finished GetMSB

--- MyBitManipulation.c
+++ MyBitManipulation.c
@@ -20,7 +20,19 @@ int ToggleBit(int num, int pos) {
 */
 int GetMSB(int num) {
    /* TODO: implement */
-   return -1;
+   int result=-1;
+
+   
+
+   while (num>0){
+      num=num/2;
+      result++;
+   }
+   if (result==-1){
+      return result;
+   }
+
+   return result;
 }
 
 /*
