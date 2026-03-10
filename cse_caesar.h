b1ae0f1cc0c19d3d0f78210a209f953d87cb8cb0
gordonzhang-sbu <gordon.zhang@stonybrook.edu>
2026-03-10 20:45:11 +0000

    Restart

--- cse_caesar.h
+++
@@ -1,7 +0,0 @@
-#ifndef CSE_CAESAR_H
-#define CSE_CAESAR_H
-
-int encryptCaesar(const char *plaintext, char *ciphertext, int key);
-int decryptCaesar(const char *ciphertext, char *plaintext, int key);
-
-#endif
