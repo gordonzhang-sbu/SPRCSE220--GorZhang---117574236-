e5668d9fe65548c2a57fb211c5b43b15fd445606
gordonzhang-sbu <gordon.zhang@stonybrook.edu>
2026-03-10 16:45:56 -0400

    Add files via upload

---
+++ strgPtr.c
@@ -0,0 +1,40 @@
+#include "strgPtr.h"
+/* DO NOT use <string.h> or <ctype.h> */
+
+int strgLen(const char *s) {
+    (void)s;
+    /* TODO */
+    return 0;
+}
+
+void strgCopy(const char *source, char *destination, size_t size) {
+    (void)source;
+    (void)destination;
+    (void)size;
+    /* TODO */
+}
+
+void strgChangeCase(char *s) {
+    (void)s;
+    /* TODO */
+}
+
+int strgDiff(const char *s1, const char *s2) {
+    (void)s1;
+    (void)s2;
+    /* TODO */
+    return 0;
+}
+
+void strgInterleave(const char *s1, const char *s2, char *d, size_t size) {
+    (void)s1;
+    (void)s2;
+    (void)d;
+    (void)size;
+    /* TODO */
+}
+
+void strgReverseLetters(char *s) {
+    (void)s;
+    /* TODO */
+}
