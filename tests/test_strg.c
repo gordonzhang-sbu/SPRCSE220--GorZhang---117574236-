155dd3f238ea03b5ab9f6c79f5b7aaac5b5fde9b
gordonzhang-sbu <gordon.zhang@stonybrook.edu>
2026-03-13 21:42:29 +0000

    strgLen Tests

--- tests/test_strg.c
+++ tests/test_strg.c
@@ -12,14 +12,52 @@ Test(strgLen, nullChar) {
     cr_expect_eq(strgLen('\0'), -1);
 }
 Test(strgLen, allCap) {
-    cr_expect_eq(strgLen("LENGTH"), -1);
+    cr_expect_eq(strgLen("LENGTH"), 6);
+}
+Test(strgLen, allLow) {
+    cr_expect_eq(strgLen("length"), 6);
+}
+Test(strgLen, mixCapLow) {
+    cr_expect_eq(strgLen("LeNgTh"), 6);
+}
+Test(strgLen, newLine) {
+    cr_expect_eq(strgLen("\n"), 1);
+}
+Test(strgLen, specialChar) {
+    cr_expect_eq(strgLen(".[,]/?"), 6);
+}
+Test(strgLen, space) {
+    cr_expect_eq(strgLen(" "), 1);
+}
+Test(strgLen, number) {
+    cr_expect_eq(strgLen("123924167431746172361824781372"), 30);
+}
+Test(strgLen, mixCharNumSpecial) {
+    cr_expect_eq(strgLen("This is CSE 220, I like C programming. :)"), 41);
+}
+Test(strgLen, empty) {
+    cr_expect_eq(strgLen(""), 0);
 }
 
+
+
+
+
+//strgCopy tests
 Test(strgCopy, truncation) {
     char d[5];
     strgCopy("Computer Science", d, 5);
     cr_expect_str_eq(d, "Comp");
 }
+
+
+
+
+
+
+
+
+
 
 Test(strgChangeCase, adjacency) {
     char s[] = "Stony Brook";
